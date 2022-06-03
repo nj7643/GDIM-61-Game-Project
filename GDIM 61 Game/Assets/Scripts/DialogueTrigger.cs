d961b72fe6aa744b4a810ad6348b75fed7385903
Keegan Najera <keganrodriguez@gmail.com>
2022-06-03 11:59:04 -0700

    dialogue test

--- GDIM 61 Game/Assets/Scripts/DialogueTrigger.cs
+++ GDIM 61 Game/Assets/Scripts/DialogueTrigger.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+    public string player; 
 
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == player)
+        {
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        }
+        
     }
 }
