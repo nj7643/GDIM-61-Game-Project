b491ad78d0b6f27cdaf9ef89f7e5d3f749953b81
Joshua Nunez <69788629+nj7643@users.noreply.github.com>
2022-06-07 10:56:42 -0700

    adjusted moving platforms

---
+++ GDIM 61 Game/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private float directionTimer = 2.0f;
+    [SerializeField] private float moveSpeed = 5.0f;
+    private float currentTime;
+
+    private bool directionSwitch = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = directionTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (directionSwitch)
+        {
+            transform.Translate(0, moveSpeed * Time.deltaTime, 0);
+        }
+        else
+        {
+            transform.Translate(0, -moveSpeed * Time.deltaTime, 0);
+        }
+
+        if (currentTime > 0f)
+        {
+            currentTime -= Time.deltaTime;
+        }
+        else
+        {
+            ChangeDirections();
+            currentTime = directionTimer;
+        }
+
+
+
+    }
+
+    void ChangeDirections()
+    {
+        if (directionSwitch)
+        {
+            directionSwitch = false;
+        }
+        else
+        {
+            directionSwitch = true;
+        }
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            other.transform.SetParent(gameObject.transform);
+            Debug.Log("Success!");
+        }
+
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            other.transform.SetParent(null);
+            Debug.Log("Exit!");
+        }
+
+    }
+
+}
